15ffc61b611c776b0890c6d0313421789608f627
Milan <milan.mirkovic@bsci.com>
2026-03-16 15:28:54 +0100

    adding cognito

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '/api',
+  apiBaseUrl: 'https://i7cggvdtsm.us-east-1.awsapprunner.com/api',
   cognito: {
     userPoolId: 'us-east-1_KiB8SY9mN',
     userPoolClientId: '3npjs7hkkjad3tnleo21q2imvi',
