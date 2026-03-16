93bc292dc6c40ef4eb30d92245917335507a0db4
Milan <milan.mirkovic@bsci.com>
2026-03-16 11:46:05 +0100

    push changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,3 +2,4 @@ export const environment = {
   production: false,
   apiBaseUrl: '__API_URL__'
 };
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '/api'
+  apiBaseUrl: '__API_URL__'
 };
-
